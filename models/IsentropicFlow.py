1ce46a653de54abf4c2e85e7708b9f16b574e3eb
Charles O'Neill <charles.oneill@gmail.com>
2025-02-17 21:13:43 -0600

    Adds unit tests to the Isentropic flow code Adds gitignore

--- models/IsentropicFlow.py
+++ models/IsentropicFlow.py
@@ -24,13 +24,17 @@ class IsentropicFlow:
     #could also replace as a subclass inheriting the overall isentropic flow class
     def ifr(self, val, selectedItem):
         #ToDo put in the selected item and calculate the mach number
+        pass
 
+    # Temperature Ratio: T/To = Temperature / Total Temperature
     def tt0(self,m):
         return np.pow((1.+(self.gamma-1)/2.*m*m),-1)
 
+    # Pressure Ratio: P/Po = Pressure / Total Pressure
     def pp0(self,m):
         return np.pow((1.+(self.gamma-1.)/2.*m*m),-self.gamma/(self.gamma-1.))
 
+    # Density Ratio: rho/rho_o
     def rr0(self,m):
         return np.pow((1.+(self.gamma-1.)/2*m*m),-1/(self.gamma-1.))
 
@@ -46,6 +50,7 @@ class IsentropicFlow:
     def aas(self,m):
         return 1./self.rrs(self,m)*np.sqrt(1./self.tts(self,m))/m
 
+    # Prandtl Meyer Function
     def nu(self,m):
         n = np.sqrt((self.gamma + 1.) / (self.gamma - 1.)) * np.atan(np.sqrt((self.gamma - 1.) / (self.gamma + 1.) * (m * m - 1.)))
         n = n - np.atan(np.sqrt(m * m - 1.))
@@ -56,7 +61,7 @@ class IsentropicFlow:
         return np.sqrt((1. + .5 * (self.gamma - 1.) * m1 * m1) / (self.gamma * m1 * m1 - .5 * (self.gamma - 1.)))
 
     def __call__(self):
-
+        pass
         # if (self.solverType=='ifr'):
         #     #input values, mach no, T/T0, p/p0, rho/rho0, A/A*(sub), A/A*(super), Mach angle, P-M angle
         #     def ifr(val, item):
@@ -74,3 +79,33 @@ class IsentropicFlow:
         # elif self.solverType=='rayf':
         #     #input values, mach no, T0/T0* (sup or sup), T/T*(above or below Tmax), P/P*, P0/P0*(sup or sub), U/U*, (s*-s)/R (sup or sub)
 
+
+if __name__ == "__main__":
+
+    def unittestrange(text, value, truthlow, truthhigh):
+        if truthlow <= value <= truthhigh:
+            print(f' {text} Passed')
+        else:
+            print(f' {text} Failed     Value {value}   Truth {truthlow} to {truthhigh}')
+    def unittestvec(text,value,truth, tolerance):
+        truthlow = [i*(1-tolerance) for i in truth]
+        truthhigh = [i * (1 + tolerance) for i in truth]
+        valuevec = [i  for i in value]
+        unittestrange(text,valuevec,truthlow,truthhigh)
+    def unittestscalar(text,value,truth, tolerance):
+        truthlow = truth * (1 - tolerance)
+        truthhigh =  truth * (1 + tolerance)
+        unittestrange(text, value, truthlow, truthhigh)
+
+    print(f'Unit tests of Isentropic Flow')
+    flow = IsentropicFlow()
+    # Reference Isentropic Flow Values from Gas Dynamics, James John, Table A.1
+    unittestscalar("T/To Mach 1",flow.tt0(1),0.83333, 0.01)
+    unittestscalar("T/To Mach 2", flow.tt0(2), 0.5555, 0.01)
+    unittestscalar("P/Po Mach 1", flow.pp0(1), 0.5283, 0.01)
+    unittestscalar("P/Po Mach 2", flow.pp0(2), 0.1278, 0.01)
+
+    # Reference P-M Values from Gas Dynamics, James John, Appendix D
+    unittestscalar("nu Mach 1", flow.nu(1), 0, 0.01)
+    unittestscalar("nu Mach 2", flow.nu(2), 26.380, 0.01)
+    
